75f10122ddd4ac0dc91c079978aca5a005cfac79
SongHae8640 <thdgo456@gmail.com>
2022-08-10 00:23:16 +0900

    람다를 이용하여 커맨드 클래스 생략
    - Command 인터페이스에 추상 메소드가 하나 뿐일때만 가능

--- src/test/java/commandPattern/DesignedRemoteControllerTest.java
+++ src/test/java/commandPattern/DesignedRemoteControllerTest.java
@@ -9,37 +9,27 @@ public class DesignedRemoteControllerTest {
         DesignedRemoteController remoteController = new DesignedRemoteController();
 
         Light light = new Light();
-        LightOnCommand lightOnCommand = new LightOnCommand(light);
-        LightOffCommand lightOffCommand = new LightOffCommand(light);
-        remoteController.setCommand(0,lightOnCommand, lightOffCommand);
+        remoteController.setCommand(0, () -> light.onLight(), () -> light.offLight());
         remoteController.pressOnButton(0);
         remoteController.pressOffButton(0);
 
         CeilingFan ceilingFan = new CeilingFan();
-        CeilingFanOnCommand ceilingFanOnCommand = new CeilingFanOnCommand(ceilingFan);
-        CeilingFanOffCommand ceilingFanOffCommand = new CeilingFanOffCommand(ceilingFan);
-        remoteController.setCommand(1, ceilingFanOnCommand, ceilingFanOffCommand);
+        remoteController.setCommand(1, () -> ceilingFan.high(), () -> ceilingFan.high());
         remoteController.pressOnButton(1);
         remoteController.pressOffButton(1);
 
         BathRoomLight bathRoomLight = new BathRoomLight();
-        BathLightOnCommand bathLightOnCommand = new BathLightOnCommand(bathRoomLight);
-        BathLightOffCommand bathLightOffCommand = new BathLightOffCommand(bathRoomLight);
-        remoteController.setCommand(2,bathLightOnCommand, bathLightOffCommand);
+        remoteController.setCommand(2, () -> bathRoomLight.on(), () -> bathRoomLight.off());
         remoteController.pressOnButton(2);
         remoteController.pressOffButton(2);
 
         Tv tv = new Tv();
-        TvOnCommand tvOnCommand = new TvOnCommand(tv);
-        TvOffCommand tvOffCommand = new TvOffCommand(tv);
-        remoteController.setCommand(3,tvOnCommand,tvOffCommand);
+        remoteController.setCommand(3, () -> tv.onPower(), () -> tv.offPower());
         remoteController.pressOnButton(3);
         remoteController.pressOffButton(3);
 
         LivingRoomLight livingRoomLight = new LivingRoomLight();
-        LivingRoomLightOnCommand livingRoomLightOnCommand = new LivingRoomLightOnCommand(livingRoomLight);
-        LivingRoomLightOffCommand livingRoomLightOffCommand = new LivingRoomLightOffCommand(livingRoomLight);
-        remoteController.setCommand(4,livingRoomLightOnCommand, livingRoomLightOffCommand);
+        remoteController.setCommand(4, () -> livingRoomLight.on(), () -> livingRoomLight.off());
         remoteController.pressOnButton(4);
         remoteController.pressOffButton(4);
 
