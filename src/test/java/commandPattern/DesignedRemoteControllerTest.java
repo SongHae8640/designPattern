48b3e28844e6f704c0ce4b456f1273f7669dcaa3
SongHae <thdgo456@gmail.com>
2022-08-11 11:14:28 +0900

    인보커(리모컨)에 등록하는 과정을 매서드로 분리

--- src/test/java/commandPattern/DesignedRemoteControllerTest.java
+++ src/test/java/commandPattern/DesignedRemoteControllerTest.java
@@ -5,35 +5,57 @@ import org.junit.Test;
 public class DesignedRemoteControllerTest {
 
     @Test
-    public void test(){
-        DesignedRemoteController remoteController = new DesignedRemoteController();
+    public void commandTest(){
+        DesignedRemoteController remoteController = getDesignedRemoteController();
 
-        Light light = new Light();
-        remoteController.setCommand(0, () -> light.onLight(), () -> light.offLight());
-        remoteController.pressOnButton(0);
-        remoteController.pressOffButton(0);
-
-        CeilingFan ceilingFan = new CeilingFan();
-        remoteController.setCommand(1, () -> ceilingFan.high(), () -> ceilingFan.high());
         remoteController.pressOnButton(1);
         remoteController.pressOffButton(1);
 
-        BathRoomLight bathRoomLight = new BathRoomLight();
-        remoteController.setCommand(2, () -> bathRoomLight.on(), () -> bathRoomLight.off());
         remoteController.pressOnButton(2);
         remoteController.pressOffButton(2);
 
-        Tv tv = new Tv();
-        remoteController.setCommand(3, () -> tv.onPower(), () -> tv.offPower());
         remoteController.pressOnButton(3);
         remoteController.pressOffButton(3);
 
-        LivingRoomLight livingRoomLight = new LivingRoomLight();
-        remoteController.setCommand(4, () -> livingRoomLight.on(), () -> livingRoomLight.off());
         remoteController.pressOnButton(4);
         remoteController.pressOffButton(4);
 
         System.out.println(remoteController);
 
+    }
+
+    private DesignedRemoteController getDesignedRemoteController() {
+        DesignedRemoteController remoteController = new DesignedRemoteController();
+
+        Light light = new Light();
+        LightOnCommand lightOnCommand = new LightOnCommand(light);
+        LightOffCommand lightOffCommand = new LightOffCommand(light);
+        remoteController.setCommand(0,lightOnCommand, lightOffCommand);
+        remoteController.pressOnButton(0);
+        remoteController.pressOffButton(0);
+
+        CeilingFan ceilingFan = new CeilingFan();
+        CeilingFanOnCommand ceilingFanOnCommand = new CeilingFanOnCommand(ceilingFan);
+        CeilingFanOffCommand ceilingFanOffCommand = new CeilingFanOffCommand(ceilingFan);
+        remoteController.setCommand(1, ceilingFanOnCommand, ceilingFanOffCommand);
+
+
+        BathRoomLight bathRoomLight = new BathRoomLight();
+        BathLightOnCommand bathLightOnCommand = new BathLightOnCommand(bathRoomLight);
+        BathLightOffCommand bathLightOffCommand = new BathLightOffCommand(bathRoomLight);
+        remoteController.setCommand(2,bathLightOnCommand, bathLightOffCommand);
+
+
+        Tv tv = new Tv();
+        TvOnCommand tvOnCommand = new TvOnCommand(tv);
+        TvOffCommand tvOffCommand = new TvOffCommand(tv);
+        remoteController.setCommand(3,tvOnCommand,tvOffCommand);
+
+
+        LivingRoomLight livingRoomLight = new LivingRoomLight();
+        LivingRoomLightOnCommand livingRoomLightOnCommand = new LivingRoomLightOnCommand(livingRoomLight);
+        LivingRoomLightOffCommand livingRoomLightOffCommand = new LivingRoomLightOffCommand(livingRoomLight);
+        remoteController.setCommand(4,livingRoomLightOnCommand, livingRoomLightOffCommand);
+        return remoteController;
     }
 }
