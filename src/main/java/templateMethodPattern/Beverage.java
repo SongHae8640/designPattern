216819878b668646d67dc340dfb19f2d155bfae3
SongHae8640 <thdgo456@gmail.com>
2022-08-17 00:50:25 +0900

    구상 클래스에서 오버라이드 하지 못하도록 final 로 선언

--- src/main/java/templateMethodPattern/Beverage.java
+++ src/main/java/templateMethodPattern/Beverage.java
@@ -2,11 +2,11 @@ package templateMethodPattern;
 
 public abstract class Beverage {
 
-    public void boilWater(){
+    final void boilWater(){
         System.out.println("물 끓이기");
     }
 
-    public void pourInCup(){
+    final void pourInCup(){
         System.out.println("컵에 담기");
     }
 
