905b2ce47786de8d883cb099570c0f6d577f5144
송해 <thdgo456@smartscore.kr>
2022-10-05 13:07:56 +0900

    템플릿 콜백 패턴 적용

--- src/main/java/templateCallbackPattern/Calculator.java
+++ src/main/java/templateCallbackPattern/Calculator.java
@@ -7,17 +7,17 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Calculator {
-    public int calcSum(String path) throws RuntimeException {
+
+    public Integer fileReadTemplate(String path, BufferedReaderCallback callback, int initValue){
         BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(new ClassPathResource(path).getFile()));
-            int sum = 0;
             String line;
-            while ((line = br.readLine()) != null){
-                sum += Integer.parseInt(line);
+            int result = initValue;
+            while((line = br.readLine()) != null){
+                result = callback.doWithReader(line, result);
             }
-
-            return sum;
+            return result;
         } catch (IOException e) {
             System.out.println(e.getMessage());
             throw new RuntimeException(e);
@@ -32,28 +32,25 @@ public class Calculator {
         }
     }
 
-    public int calcMultiply(String path) throws RuntimeException {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(new ClassPathResource(path).getFile()));
-            int multiply = 1;
-            String line;
-            while ((line = br.readLine()) != null){
-                multiply *= Integer.parseInt(line);
+    public int calcSum(String path) throws RuntimeException {
+        BufferedReaderCallback callback = new BufferedReaderCallback() {
+            @Override
+            public int doWithReader(String line, int result) {
+                return result + Integer.parseInt(line);
             }
+        };
+        return fileReadTemplate(path, callback ,0);
+    }
 
-            return multiply;
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            throw new RuntimeException(e);
-        }finally {
-            if(br != null){
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+    public int calcMultiply(String path) throws RuntimeException {
+
+        BufferedReaderCallback callback = new BufferedReaderCallback() {
+            @Override
+            public int doWithReader(String line, int result) {
+                return result * Integer.parseInt(line);
             }
-        }
+        };
+
+        return fileReadTemplate(path, callback ,1);
     }
 }
